ad49d7fd2b5414423774e6b94aa42d0f3915cac8
Bhumit717 <bhumitnasit1@gmail.com>
2026-02-17 23:37:11 +0530

    Update Firebase config to mark-overseas and fix rules deployment

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDTx5DTprVwp7za5cZow4jTw_wtNSR6DCA",
-    authDomain: "patel-impex.firebaseapp.com",
-    projectId: "patel-impex",
-    storageBucket: "patel-impex.firebasestorage.app",
-    messagingSenderId: "579090779018",
-    appId: "1:579090779018:web:4cfaff7ff6be54fcc39673",
-    measurementId: "G-2V84H3BM6L"
+    apiKey: "AIzaSyAtWGC2M5CqAhDK1O7mVqYvkhCqXhv0Ii0",
+    authDomain: "mark-overseas.firebaseapp.com",
+    projectId: "mark-overseas",
+    storageBucket: "mark-overseas.firebasestorage.app",
+    messagingSenderId: "558178626256",
+    appId: "1:558178626256:web:c543cdd71c3e29019cccd5",
+    measurementId: "G-48YT3FTPSP"
 };
 
 // Initialize Firebase
